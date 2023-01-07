3690430935bf18bb59f0a1a7b3a108a82a5c5fe8
chrispaganon <info@chrispaganon.com>
2023-01-07 16:35:47 -0500

    Enable hover text effect on mobile scroll

--- wp-content/themes/pqe-child-theme/assets/js/pqe-general.js
+++ wp-content/themes/pqe-child-theme/assets/js/pqe-general.js
@@ -1,5 +1,16 @@
-jQuery(document).ready(function ($) {    
-  $('.pqe-image-with-overlay').on('mouseenter mouseleave', function() {
-    $(this).toggleClass('pqe-image-hovered');
-  });  
+jQuery(document).ready(function ($) {
+  window.addEventListener('scroll', function() {
+    $('.pqe-image-with-overlay').each(function () {
+      console.log($(this));
+      const position = $(this)[0].getBoundingClientRect();
+      const centerBoxHeight = Math.min(2 * position.height, window.innerHeight) - 5;
+      const distToCenterBox = ( window.innerHeight - centerBoxHeight ) / 2;
+
+      if ( position.top > distToCenterBox && position.bottom < (window.innerHeight - distToCenterBox) ) {
+        $(this)[0].classList.add('viewportVisible');
+      } else {
+        $(this)[0].classList.remove('viewportVisible');
+      }
+    });
+  }); 
 });
